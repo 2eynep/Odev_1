03a6618fab8272e12355b28f2590c4e388f21759
2eynep <92038340+2eynep@users.noreply.github.com>
2022-11-14 11:08:10 +0300

    Update Odev1.java

--- src/Odev1.java
+++ src/Odev1.java
@@ -6,7 +6,6 @@ public class Odev1 {
             try {
                 System.out.println("I'm busy...");
                 Thread.sleep(500);
-                Runtime.getRuntime().removeShutdownHook(ms);
             } catch (InterruptedException ex) {
                 System.out.println("System interrputed");
             }
